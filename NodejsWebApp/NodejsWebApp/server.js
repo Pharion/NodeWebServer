8a62a95289b731b89c199b939b6faa21835f417d
unknown <stephen@Automation.commontime.com>
2015-10-05 15:31:16 +0100

    version one

---
+++ NodejsWebApp/NodejsWebApp/server.js
@@ -0,0 +1,10 @@
+﻿var http = require('http');
+var express = require('express');
+var app = express();
+
+app.get("/", function (req, res) {
+	res.send("<html><body><h1>Express</h1></body></html>");
+});
+
+var server = http.createServer(app);
+server.listen(3000);
